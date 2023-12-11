e7372331ad10f97f0f32eff50b2672186824f059
ronneyTorres <ronneypris26@gmail.com>
2023-12-11 07:58:59 -0300

    Foi adicionado a estrutura de repetição for, para atualizações de xp do herói

--- index.js
+++ index.js
@@ -1,23 +1,24 @@
 const nomeDoHeroi = "Apollo";
-let xpDoHeroi = 10005;
+let xpDoHeroi = 0;
 let nivelDoHeroi = "";
 
-if(xpDoHeroi <= 1000) {
-  nivelDoHeroi = "Ferro";
-} else if(xpDoHeroi > 1000 && xpDoHeroi <= 2000) {
-  nivelDoHeroi = "Bronze";
-} else if(xpDoHeroi > 2000 && xpDoHeroi <= 5000) {
-  nivelDoHeroi = "Prata";
-} else if(xpDoHeroi > 5000 && xpDoHeroi <= 7000) {
-  nivelDoHeroi = "Ouro";
-} else if(xpDoHeroi > 7000 && xpDoHeroi <= 8000) {
-  nivelDoHeroi = "Platina";
-} else if(xpDoHeroi > 8000 && xpDoHeroi <= 9000) {
-  nivelDoHeroi = "Ascendente";
-} else if(xpDoHeroi > 9000 && xpDoHeroi <= 10000) {
-  nivelDoHeroi = "Imortal";
-} else {
-  nivelDoHeroi = "Radiante";
+for(xpDoHeroi; xpDoHeroi < 13000; xpDoHeroi = xpDoHeroi + 1000) {
+  if(xpDoHeroi <= 1000) {
+    nivelDoHeroi = "Ferro";
+  } else if(xpDoHeroi > 1000 && xpDoHeroi <= 2000) {
+    nivelDoHeroi = "Bronze";
+  } else if(xpDoHeroi > 2000 && xpDoHeroi <= 5000) {
+    nivelDoHeroi = "Prata";
+  } else if(xpDoHeroi > 5000 && xpDoHeroi <= 7000) {
+    nivelDoHeroi = "Ouro";
+  } else if(xpDoHeroi > 7000 && xpDoHeroi <= 8000) {
+    nivelDoHeroi = "Platina";
+  } else if(xpDoHeroi > 8000 && xpDoHeroi <= 9000) {
+    nivelDoHeroi = "Ascendente";
+  } else if(xpDoHeroi > 9000 && xpDoHeroi <= 10000) {
+    nivelDoHeroi = "Imortal";
+  } else {
+    nivelDoHeroi = "Radiante";
+  }
+  console.log(`O herói ${nomeDoHeroi}, tem um total de ${xpDoHeroi}XP classificado com nível ${nivelDoHeroi}`);
 }
-
-console.log(`O herói ${nomeDoHeroi}, tem um total de ${xpDoHeroi}XP classificado com nível ${nivelDoHeroi}`)
